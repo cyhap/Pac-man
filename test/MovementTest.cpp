b430f652bf0933d01a12bc66954b99c20f035e52
EthanQuist <ethantquist@gmail.com>
2019-11-27 10:08:17 -0800

    [Task 2.03][MovementTest.cpp] Included ROS node for test

--- test/MovementTest.cpp
+++ test/MovementTest.cpp
@@ -49,3 +49,11 @@ TEST(Movement, GetAndSet) {
   ASSERT_EQ(MoveObj.checkVisuals(), true);
 
 }
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "movement_test");
+  ros::NodeHandle nh;
+  return RUN_ALL_TESTS();
+}
