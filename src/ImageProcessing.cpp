764d09f0a3a2b9588fd91ef8f7f2c06272756a93
Corbyn Yhap <cyhap12@gmail.com>
2019-11-27 22:07:45 -0500

    [Task 2.04][ImageProcessing.cpp] ImageProcessing Stub
    Added stub so tests for this class can be written.

---
+++ src/ImageProcessing.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2019, Ari Kupferberg, Ethan Quist, Corbyn Yhap
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+ /**
+ * @file ImageProcessing.cpp
+ * @copyright 2019 Corbyn Yhap
+ * @author Corbyn Yhap
+ * @date 11/27/2019
+ * @brief This Class defines the functions that process RGB and Depth sensor
+ *        data together.
+ */
+
+
+#include "ImageProcessing.hpp"
+
+#include "GoodObject.hpp"
+
+ImageProcessing::ImageProcessing() {
+}
+
+ImageProcessing::~ImageProcessing() {
+}
+
+std::shared_ptr<Object> ImageProcessing::identifyObject(const cv::Mat &aImage) {
+  // Fixme[Yhap] Fill in this function;
+  (void) aImage;
+  std::shared_ptr<Object> tReturn(new GoodObject);
+  return tReturn;
+}
+
+std::pair<const cv::Mat&, std::shared_ptr<Object> > ImageProcessing::applyMask(
+    const cv::Mat &aImage) {
+  // Fixme[Yhap] Fill in this function;
+  (void) aImage;
+  std::pair<const cv::Mat&, std::shared_ptr<Object> > tReturn;
+  return tReturn;
+}
+
+std::pair<size_t, size_t> ImageProcessing::computeCentroid(
+    const cv::Mat &aImage) {
+  // Fixme[Yhap] Fill in this function;
+  (void) aImage;
+  std::pair<size_t, size_t> tReturn;
+  return tReturn;
+}
+void ImageProcessing::computePose(const cv::Mat &aImage,
+                                  std::pair<size_t, size_t> aPixelLoc,
+                                  std::shared_ptr<Object> aObject) {
+  // Fixme[Yhap] Fill in this function;
+  (void) aImage;
+  (void) aPixelLoc;
+  (void) aObject;
+}
